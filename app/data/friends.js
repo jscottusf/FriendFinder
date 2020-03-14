aa607f94d8f452da121c957b51048610be224695
Joel Scott <jscottusf@gmail.com>
2020-03-14 15:07:50 -0400

    matching completed using Math.min(...)

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "name": "Barack Obama",
+        "photo": "https://s.yimg.com/ny/api/res/1.2/xqweg9GZJsh0aC1CMRBqJg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTcwNTtoPTQ5Ny4wODAwNzgxMjU-/https://s.yimg.com/uu/api/res/1.2/6MizxcYxJdxXKwnQchKUfg--~B/aD0xNDQ0O3c9MjA0ODtzbT0xO2FwcGlkPXl0YWNoeW9u/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2020-03/a677f200-6502-11ea-bfb9-9a16389b4939",
         "scores": [
         "5",
         "1",
@@ -16,8 +16,8 @@ let friends = [
         ]
     },
     {
-        "name": "Jacob Deming",
-        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "name": "Joe Biden",
+        "photo": "https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQwODQ0Nzc0MjIyNDczMDA2/joe-biden-official-portrait_1600jpg.jpg",
         "scores": [
         "4",
         "2",
@@ -32,8 +32,8 @@ let friends = [
         ]
     },
     {
-        "name": "Jeremiah Scanlon",
-        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "name": "Jennifer Lawrence",
+        "photo": "https://media1.popsugar-assets.com/files/thumbor/i63Jb8lKHjsRWeu4a9IKpAHZ1go/364x0:4636x4272/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/08/28/717/n/1922398/274cf0545d66a7fd9b2956.87657545_/i/Jennifer-Lawrence.jpg",
         "scores": [
         "5",
         "2",
@@ -48,8 +48,8 @@ let friends = [
         ]
     },
     {
-        "name": "Louis T. Delia",
-        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "name": "Kelly Clarkson",
+        "photo": "https://api.time.com/wp-content/uploads/2015/04/kelly-clarkson.jpg",
         "scores": [
         "3",
         "3",
@@ -64,8 +64,8 @@ let friends = [
         ]
     },
     {
-        "name": "Lou Ritter",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "name": "Scarlett Johansson",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/6/60/Scarlett_Johansson_by_Gage_Skidmore_2_%28cropped%29.jpg",
         "scores": [
         "4",
         "3",
@@ -80,8 +80,8 @@ let friends = [
         ]
     },
     {
-        "name": "Jordan Biason",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "name": "Toby Keith",
+        "photo": "https://www.bing.com/th?id=OIP.yENHcgwFyzjendCGk0fo_QHaHa&pid=Api&rs=1",
         "scores": [
         "4",
         "4",
@@ -97,7 +97,7 @@ let friends = [
     },
     {
         "name": "Guy Ferarri",
-        "photo": "https://ih0.redbubble.net/image.418481630.7947/mwo,x1000,ipad_2_snap-pad,750x1000,f8f8f8.jpg",
+        "photo": "https://pbs.twimg.com/profile_images/991380156013920256/PZRGsMjW.jpg",
         "scores": [
         "5",
         "5",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,23 +6,21 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        var userScores = req.body.scores;
-        console.log(userScores[0]);
-
-        friends.push(req.body);
-        // var absArr = [];
-        // for (var i = 0; i < (friends.length - 1); i++) {
-        //     var userScores = friends[friends.length].scores;
-        //     var scores = friends[i].scores;
-        //     var difference = 0
-        //     for (var j = 0; j < scores.length; j++) {
-        //         difference += Math.abs(scores[j] - userScores[j]);
-        //     }
-        //     absArr.push(difference);
-        // }
-        // console.log(absArr);
-        // console.log(Math.min(parseInt(absArr)));
-        // console.log(absArr.indexOf(parseInt(absArr)));
-        // res.json(friends[absArr.indexOf(Math.min(parseInt(absArr)))]);
+        var newUser = req.body;
+        var newUserScores = req.body.scores;
+        var absArr = [];
+        for (var i = 0; i < friends.length; i++) {
+            var databaseUserScores = friends[i].scores;
+            var difference = 0;
+            for (var j = 0; j < databaseUserScores.length; j++) {
+                difference += (Math.abs(databaseUserScores[j] - newUserScores[j]))
+            }
+            absArr.push(difference);
+        }
+        var lowestAbs = Math.min(...absArr);
+        var match = absArr.indexOf(lowestAbs);
+        
+        res.json(friends[match]);
+        friends.push(newUser);
     })
 }

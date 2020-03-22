c4bf528185274ac081ae0ce0acd4d8f130eaf388
Joel Scott <jscottusf@gmail.com>
2020-03-22 15:12:36 -0400

    added some funky fresh css and made the welcome page nicer

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app/public"));
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
